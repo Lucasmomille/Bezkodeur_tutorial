35c9020bafb29c0780d8d0da573a4ce4c3032bf2
Lucas <lucas.moquet95@gmail.com>
2021-05-12 10:38:38 +0200

    add xlsx

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+
+/* For development , you may need to drop existing 
+tables and re-sync database. Just use force: true as following code
+ db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  }); */
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+});
+
+
+require("./app/routes/tutorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
